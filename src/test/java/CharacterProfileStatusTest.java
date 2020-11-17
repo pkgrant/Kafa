e9e1ce865b64b849f4cb7bfa575e03a3f3755bb8
Parker Grant <pgrant2332@gmail.com>
2020-11-17 02:04:56 -0500

    Started testing json parsing capability

---
+++ src/test/java/CharacterProfileStatusTest.java
@@ -0,0 +1,31 @@
+import DTOs.CharacterProfileStatus;
+import Util.JsonParserUtil;
+import Util.TestUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test to make sure CharacterProfileStatus is properly loaded from mock data
+ * This test assumes the mock character is a valid WoW character.
+ */
+public class CharacterProfileStatusTest {
+
+    String characterProfileStatusJson;
+
+    @Before
+    public void initialize() {
+        characterProfileStatusJson = TestUtil.jsonFileToString("./src/test/resources/CharacterProfileStatus.json");
+    }
+
+    @Test
+    public void testJsonParser() {
+
+        CharacterProfileStatus status = (CharacterProfileStatus) JsonParserUtil.parseJson(characterProfileStatusJson, CharacterProfileStatus.class);
+
+        assertTrue(status.isValid());
+        assertNotEquals(-1, status.getId());
+    }
+
+}
---
+++ src/test/java/Util/TestUtil.java
@@ -0,0 +1,20 @@
+package Util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class TestUtil {
+    public static String jsonFileToString(String file) {
+        Path path = Paths.get(file);
+
+        try {
+            return Files.readString(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Error processing file.";
+    }
+}
